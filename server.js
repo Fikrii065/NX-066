f1dc3739561d7d27cc10c845af3270920c19cf54
Fikrii065 <fikkry247@gmail.com>
2026-04-12 21:45:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 const path      = require('path');
 
 const app  = express();
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // ── Middleware ──────────────────────────────────────────────────────────────
